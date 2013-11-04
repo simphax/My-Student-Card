2eaacaac6719a53ec5fe6c06ff6c2a2486c3b77f
simphax <sim.nilsson@gmail.com>
2013-11-04 18:22:35 +0100

    Initial Commit

---
+++ My Student Card/SHXAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SHXAppDelegate.h
+//  My Student Card
+//
+//  Created by Simon on 2013-11-04.
+//  Copyright (c) 2013 Simphax. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SHXAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ My Student Card/SHXViewController.m
@@ -0,0 +1,29 @@
+//
+//  SHXViewController.m
+//  My Student Card
+//
+//  Created by Simon on 2013-11-04.
+//  Copyright (c) 2013 Simphax. All rights reserved.
+//
+
+#import "SHXViewController.h"
+
+@interface SHXViewController ()
+
+@end
+
+@implementation SHXViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
